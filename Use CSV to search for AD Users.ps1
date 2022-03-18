6bdc1697e7823ad5fa6028f8b1058c0eddadb4ae
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-18 10:26:04 -0400

    Add files via upload

---
+++ Use CSV to search for AD Users.ps1
@@ -0,0 +1,13 @@
+﻿foreach ($user in (Import-Csv -Path "C:\Users\temp\VPNusers.csv")) {
+    $Name = $user
+    $SurName = $user.SurName
+    $UPN =$user.UPN
+
+$adUsers = Get-AdUser -Filter {(givenname -eq $GivenName) -and (surname -eq $SurName)}
+if ($adUsers){
+$adUsers | select -Property GivenName, SurName, UserPrincipalName | Format-Table -AutoSize -Wrap
+    }
+else{
+Write-Host "$GivenName $Surname was not found in AD."
+    }
+}
