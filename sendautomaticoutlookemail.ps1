9db74c7e8d2b1695e3b3c6f1d46858ba448e7f55
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-18 10:21:55 -0400

    Add files via upload

---
+++ sendautomaticoutlookemail.ps1
@@ -0,0 +1,13 @@
+﻿#create COM object named Outlook
+$Outlook = New-Object -ComObject Outlook.Application 
+#create Outlook MailItem named Mail using CreateItem() method
+$Mail = $Outlook.CreateItem(0)
+#add properties as desired
+$Mail.To = "test@test.com"
+$Mail.Subject = "subject"
+$Mail.Body = "testing"
+#send message
+Send-MailMessage $Mail.Send() 
+#quit and cleanup
+$Outlook.Quit()
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
